c8b222c632b0f453798a35405ebbf8d0b7e93e76
pepperice <hkri@protonmail.com>
2018-06-27 16:06:50 +0800

    Add initial word dictionary and loading it to the view.

--- res/js/app.js
+++ res/js/app.js
@@ -1,11 +1,44 @@
 var app = new Vue({
     el: '#app',
     data: {
-        screen: 0
+        screen: 0,
+        currentWord: null
     },
     methods: {
+        /**
+         * Update the current screen state of the app using a specified index.
+         */
         goToScreen: function (screenIndex) {
             this.screen = screenIndex;
+            onScreenChange(screenIndex);
         }
     }
 });
+
+/**
+ * Handler for screen change event.
+ * @param {*} screenIndex - The index of the next screen.
+ */
+function onScreenChange(screenIndex) {
+    if (screenIndex == 1) {
+        app.currentWord = nextWord();
+    }
+}
+
+/**
+ * Picks a random word from the vocabulary dictionary.
+ */
+function nextWord() {
+    var dictionarySize = dictionary.length;
+    var randomIndex = Math.floor(Math.random() * dictionarySize);
+    return dictionary[randomIndex];
+}
+
+/**
+ * Check whether the player's answer is included in the array of possible answers.
+ * @param {*} dictionaryWord - the dictionary word to check.
+ * @param {string} actualAnswer - the player's actual answer.
+ */
+function checkAnswer(dictionaryWord, actualAnswer) {
+    // TODO: verify answer.
+}
---
+++ res/js/dictionary.js
@@ -0,0 +1,56 @@
+var dictionary = [
+    {
+        key: '日',
+        answer: [
+            'day',
+            'sun'
+        ]
+    },
+    {
+        key: '本',
+        answer: [
+            'book',
+            'this',
+            'tome',
+            'volume',
+            'main',
+            'principal'
+        ]
+    },
+    {
+        key: '雨',
+        answer: [
+            'rain',
+        ]
+    },
+    {
+        key: '月',
+        answer: [
+            'month',
+            'moon'
+        ]
+    },
+    {
+        key: '人',
+        answer: [
+            'man',
+            'person',
+            'people',
+            'mankind',
+            'human being',
+            'adult',
+        ]
+    },
+    {
+        key: '中',
+        answer: [
+            'middle',
+            'center',
+            'midway',
+            'halfway',
+            'core',
+            'emphasis',
+            'midst'
+        ]
+    },
+];
